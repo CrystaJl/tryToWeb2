ea3b4b961b223ab31627a7a2f3fbe7f412a4f32e
LinHidden <linthehidden@gmail.com>
2024-05-24 01:23:45 +0300

    graphs!!!!

--- static/dataRandomizer.py
+++ static/dataRandomizer.py
@@ -3,10 +3,10 @@ import json
 
 def generate_data():
     data = []
-    for i in range(10):
+    for i in range(4):
         data.append({
             'label': f'Data {i}',
-            'values': [random.randint(0, 100) for _ in range(10)],
+            'values': [random.randint(0, 100) for _ in range(4)],
             'color': f'#{random.randint(0, 0xFFFFFF):06x}'
         })
     return data
